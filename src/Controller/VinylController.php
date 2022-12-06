25270fdcf09c9d058106aa840738daf329b5e1a6
Danyk <danykdev@gmail.com>
2022-12-06 20:49:24 +0300

    Add new template

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
   #[Route('/', name: 'homepage')]
   public function index(): Response
   {
-    return new Response('Title: PB and Jams');
+    $tracks = [
+      ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+      ['song' => 'Waterfalls', 'artist' => 'TLC'],
+      ['song' => 'Kiss from a Rose', 'artist' => 'Seal']
+    ];
+
+    return $this->render('vinyl/homepage.html.twig', [
+      'title' => 'PB & Jams',
+      'tracks' => $tracks
+    ]);
   }
 
   #[Route('/browse/{genre}', name: 'browse_genre')]
